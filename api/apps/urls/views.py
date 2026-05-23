1670561eb2cd0d62178fa9126409562d2ce4e931
KapustyakYuriy <yuriy.kapustyak.w@gmail.com>
2026-05-24 00:17:11 +0300

    add ShortURL ViewSet with owner filtering and click_count annotation

--- api/apps/urls/views.py
+++ api/apps/urls/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.db.models import Count, QuerySet
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.serializers import BaseSerializer
 
-# Create your views here.
+from apps.urls.serializers import ShortURLSerializer
+from apps.urls.models import ShortURL
+from apps.urls.utils import generate_short_code
+
+class ShortURLViewSet(ModelViewSet):
+	serializer_class = ShortURLSerializer
+
+	def get_queryset(self)->QuerySet:
+		return ShortURL.objects.filter(owner=self.request.user).annotate(
+			click_count=Count("clickevent")
+		)
+
+	def perform_create(self, serializer: BaseSerializer)->None:
+		serializer.save(
+			owner=self.request.user,
+			short_code=generate_short_code(),
+		)
