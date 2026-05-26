986958f23034f7be82f46ef9167457eaaffe2949
KapustyakYuriy <yuriy.kapustyak.w@gmail.com>
2026-05-27 00:07:57 +0300

    add ClickEventSerializer and ShortURLDetailSerializer with recent clicks

--- api/apps/urls/serializers.py
+++ api/apps/urls/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
+
 from apps.urls.models import ShortURL
+from apps.analytics.models import ClickEvent
+
+class ClickEventSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = ClickEvent
+		fields = ["clicked_at", "ip_address", "browser", "os", "device_type"]
 
 class ShortURLSerializer(serializers.ModelSerializer):
 	click_count = serializers.IntegerField(read_only=True)
@@ -9,3 +16,17 @@ class ShortURLSerializer(serializers.ModelSerializer):
 
 		fields = ["id", "original_url", "short_code", "created_at", "click_count"]
 		read_only_fields = ["short_code", "created_at"]
+
+class ShortURLDetailSerializer(ShortURLSerializer):
+	recent_clicks = serializers.SerializerMethodField()
+
+	class Meta(ShortURLSerializer.Meta):
+		fields = ShortURLSerializer.Meta.fields + ["recent_clicks"]
+
+	def get_recent_clicks(self, obj):
+		clicks = getattr(obj, "prefetched_clicks", None)
+		if clicks is None:
+			clicks = obj.clickevent_set.order_by("-clicked_at")[:10]
+		else:
+			clicks = clicks[:10]
+		return ClickEventSerializer(clicks, many=True).data
