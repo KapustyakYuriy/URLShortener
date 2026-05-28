18673d27b5a32843e13d34c5d7fcb28c9cee261e
KapustyakYuriy <yuriy.kapustyak.w@gmail.com>
2026-05-29 01:22:21 +0300

    start subscriber as daemon thread in AppConfig.ready()

--- api/apps/analytics/apps.py
+++ api/apps/analytics/apps.py
@@ -1,5 +1,13 @@
+import sys
+import threading
 from django.apps import AppConfig
-
 
 class AnalyticsConfig(AppConfig):
     name = "apps.analytics"
+
+    def ready(self):
+        if "runserver" not in sys.argv and "gunicorn" not in sys.argv[0]:
+            return
+        from apps.analytics.subscriber import run_subscriber
+        thread = threading.Thread(target=run_subscriber, daemon=True)
+        thread.start()
